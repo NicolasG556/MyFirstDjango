8d351b5c40bd73165f677a93b852b29735f2c5e8
NicolasGuingouain <nicolas.guingouain@gmail.com>
2023-09-09 00:28:23 +0200

    Model et View

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+# ~/projects/django-web-app/merchex/listings/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
+
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+            <h1>Liste des annonces : </h1>
+            <ul>
+                <li>{listings[0].title}</li>
+                <li>{listings[1].title}</li>
+                <li>{listings[2].title}</li>
+                <li>{listings[3].title}</li>
+            </ul>
+    """)
+
+def contact(request):
+    return HttpResponse('<h1>Contact Us</h1> <p>Nous placerons ici le formulaire pour nous contacter</p>')
