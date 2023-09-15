e190e135232a6a572222858397eae4f8617b5c5d
NicolasGuingouain <nicolas.guingouain@gmail.com>
2023-09-15 15:29:55 +0200

    gabarit + CSS + CRUD

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+from listings.models import Listing
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+admin.site.register(Band, BandAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
+
+class ListingAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('title', 'sold', 'type') # liste les champs que nous voulons sur l'affichage de la liste
+
+admin.site.register(Listing, ListingAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
