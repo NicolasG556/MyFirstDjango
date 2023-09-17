810e75b495f78dfac7a0cf09ece9ea02473d9307
NicolasGuingouain <nicolas.guingouain@gmail.com>
2023-09-17 14:06:37 +0200

    Ajout Read + Forms Create, Update

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,10 @@
 # ~/projects/django-web-app/merchex/listings/views.py
-
+from django.core.mail import send_mail
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm, BandForm, ListingForm
 
 
 def band_list(request):
@@ -12,15 +13,54 @@ def band_list(request):
                   'listings/band_list.html',
                   {'bands': bands})
 
+
 def band_detail(request, id):
     band = Band.objects.get(id=id)
     return render(request,
                   'listings/band_detail.html',
                   {'band': band})
 
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            # créer une nouvelle « Band » et la sauvegarder dans la db
+            band = form.save()
+            # redirige vers la page de détail du groupe que nous venons de créer
+            # nous pouvons fournir les arguments du motif url comme arguments à la fonction de redirection
+            return redirect('band-detail', band.id)
+
+    else:
+        form = BandForm()
+
+    return render(request,
+                  'listings/band_create.html',
+                  {'form': form})
+
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+
 def about(request):
     return render(request,
                   'listings/about.html')
+
 
 def listings(request):
     listings = Listing.objects.all()
@@ -28,11 +68,76 @@ def listings(request):
                   'listings/listings.html',
                   {'listings': listings})
 
+
 def listing_detail(request, id):
     listing = Listing.objects.get(id=id)
     return render(request,
                   'listings/listing_detail.html',
                   {'listing': listing})
 
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            # créer une nouvelle « Band » et la sauvegarder dans la db
+            listing = form.save()
+            # redirige vers la page de détail du groupe que nous venons de créer
+            # nous pouvons fournir les arguments du motif url comme arguments à la fonction de redirection
+            return redirect('listing-detail', listing.id)
+
+    else:
+        form = ListingForm()
+
+    return render(request,
+                  'listings/listing_create.html',
+                  {'form': form})
+
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('band-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(request,
+                  'listings/listing_update.html',
+                  {'form': form})
+
+
 def contact(request):
-    return HttpResponse('<h1>Contact Us</h1> <p>Nous placerons ici le formulaire pour nous contacter</p>')
+    if request.method == 'POST':
+        # créer une instance de notre formulaire et le remplir avec les données POST
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+            return redirect('email-sent')  # ajoutez cette instruction de retour
+
+    # si le formulaire n'est pas valide, nous laissons l'exécution continuer jusqu'au return
+    # ci-dessous et afficher à nouveau le formulaire (avec des erreurs).
+
+    else:
+        # ceci doit être une requête GET, donc créer un formulaire vide
+        form = ContactUsForm()
+
+    return render(request,
+                  'listings/contact.html',
+                  {'form': form})
+
+
+def email_sent(request):
+    return render(request,
+                  'listings/email_sent.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -19,11 +19,16 @@ from django.urls import path
 from listings import views
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('bands/', views.band_list, name='band-list'),
-path('bands/<int:id>/', views.band_detail, name='band-detail'),
-path('about-us/', views.about),
-path('listings/', views.listings, name='listing-list'),
-path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
-path('contact-us/', views.contact)
+    path('admin/', admin.site.urls),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
+    path('bands/add/', views.band_create, name='band-create'),
+    path('bands/<int:id>/update', views.band_update, name='band-update'),
+    path('about-us/', views.about, name='about-us'),
+    path('listings/', views.listings, name='listing-list'),
+    path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
+    path('listing/add/', views.listing_create, name='listing-create'),
+    path('listing/<int:id>/update', views.listing_update, name='listing-update'),
+    path('contact-us/', views.contact, name='contact'),
+    path('email_sent/', views.email_sent, name='email-sent')
 ]
